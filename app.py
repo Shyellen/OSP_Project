ba41575628ca3ba334a094bb8be94a1f1b80d9c9
Hyelin Seok <50590132+Shyellen@users.noreply.github.com>
2020-06-06 15:16:59 +0900

    app.py: simple version for receiving url texts

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/fileUpload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save('/home/hyelin/uploads/' + secure_filename(f.filename))
+        return 'success'
+    return 'file uploading failed'
+
+@app.route('/urlSending', methods=['GET', 'POST'])
+def text_recv():
+    if request.method == 'POST':
+        inTXT = request.form['url']
+        return(inTXT)
+    return("url receiving failed")

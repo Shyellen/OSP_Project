094311d7814bc40c5b825edd71416c4f9becdd0a
Hyelin Seok <50590132+Shyellen@users.noreply.github.com>
2020-06-28 02:01:08 +0900

    app.py: add analysis function

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, request
-import requests
-import time
-import operator
-from bs4 import BeautifulSoup
 from elasticsearch import Elasticsearch
+from bs4 import BeautifulSoup
 import urllib.request
+import requests
+import operator
+import numpy
+import time
+import math
+
+
+app = Flask(__name__)
 
 es_host = "127.0.0.1"
 es_port = "9200"
 es = Elasticsearch([{'host':es_host, 'port':es_port}], timeout=30)
 es_id = 0
 
-app = Flask(__name__)
-total_result = []
-error_message = "<h1>[ERROR]</h1><h2>Input data is invalid.</h2>" \
+total_result    = []
+word_list       = []
+word_dict       = {}
+url_error_message = "<h1>[ERROR]</h1><h2>Input data is invalid.</h2>" \
                 "1. Input may have been added as a duplicate.<br>" \
                 "2. Input may not be url.<br>" \
                 "3. Input may not be valid."
@@ -40,19 +46,20 @@ def crawling(url):
     word += ' '
     words = word.split()
 
-    d = dict()
-    size = 0
+    w_list = []
     for w in words:
-        if w in d:
-            d[w] = d[w] + 1
+        if w in word_dict:
+            word_dict[w] = word_dict[w] + 1
         else:
-            d[w] = 1
-    return d, len(words)
+            word_dict[w] = 1
+            w_list.append(w)
+    word_list.append(w_list)
+    return w_list, len(words)
 
 
 def start_crawl(url):
     start = time.time()
-    d, size = crawling(url)
+    w_list, size = crawling(url)
     c_time = round(time.time() - start, 5)
     print("==============================")
     print("크롤링 한 주소:", url)
@@ -60,25 +67,20 @@ def start_crawl(url):
     print("크롤링 시간:", c_time)
     print("==============================")
     result = [url, size, c_time]
-    es_data_add(d, result)
+    es_data_add(w_list, result)
     return result
 
 
-def es_data_add(word_dict, result):
+def es_data_add(w_list, result):
     global es_id
-    word_dict = dict(sorted(word_dict.items(), key=operator.itemgetter(1), reverse=True))
     data = {
         "url": result[0],
         "size": result[1],
         "crawl_time": result[2],
-        "words": list(word_dict.keys()),
-        "frequencies": list(word_dict.values())
+        "words": w_list
     }
     es_id += 1
     es.index(index='web', doc_type='word', id=es_id, body=data)
-    # result = es.search(index='web')
-    # print("=========== result ===========")
-    # print(result)
 
 
 def url_validation(url):
@@ -97,6 +99,95 @@ def url_validation(url):
     return 0
 
 
+def similarity_url(url_index):
+    sim = {}
+    for i in range(0, len(word_list)):
+        if i != url_index:
+            sim[i] = cal_Cossimil(url_index, i)
+
+    sorted_sim = sorted(sim.items(), key=lambda x:x[1], reverse=True)
+    sorted_sim = sorted_sim[:3]
+    sim = dict(sorted_sim)
+
+    return sim
+
+
+def cal_tf_idf(w_list):
+    tf_idf = {}
+
+    idf_d = compute_idf()
+    tf_d = compute_tf(w_list)
+    for word, tfval in tf_d.items():
+        tf_idf[word] = tfval * idf_d[word]
+
+    sorted_tfidf = sorted(tf_idf.items(), key=lambda x:x[1], reverse=True)
+    sorted_tfidf = sorted_tfidf[:10]
+    tf_idf = dict(sorted_tfidf)
+
+    return tf_idf
+
+
+def compute_idf():
+    Dval = len(word_list)   #num of url
+    bow = set()             #build set of words
+
+    for i in range(0, len(word_list)):
+        for tok in word_list[i]:
+            bow.add(tok)
+
+    idf_d = {}
+    for t in bow:
+        cnt = 0
+        for s in word_list:
+            if t in s:
+                cnt += 1
+            if cnt == 0:
+                idf_d[t] = 0
+            else:
+                idf_d[t] = float(math.log(Dval / cnt))
+
+    return idf_d
+
+
+def compute_tf(w_list):
+    bow = set()
+    wordcount_d = {}
+
+    for tok in w_list:
+        if tok not in wordcount_d.keys():
+            wordcount_d[tok] = 0
+        wordcount_d[tok] += 1
+        bow.add(tok)
+
+    tf_d = {}
+    for word, count in wordcount_d.items():
+        tf_d[word] = float( count / len(bow))
+
+    return tf_d
+
+
+def cal_Cossimil(words1, words2):
+    v1 = make_vector(words1)
+    v2 = make_vector(words2)
+
+    dotpro = numpy.dot(v1, v2)
+    cossimil = dotpro / (numpy.linalg.norm(v1) * numpy.linalg.norm(v2))
+
+    return cossimil
+
+
+def make_vector(i):
+    v = []
+    s = word_list[i]
+    for w in word_dict.keys():
+        val = 0
+        for t in s:
+            if t == w:
+                val += 1
+        v.append(val)
+    return v
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -109,7 +200,7 @@ def text_recv():
         if url_validation(url_text) == 0:
             total_result.append(start_crawl(url_text))
             return render_template('result.html', result=total_result)
-    return error_message
+    return url_error_message
 
 
 @app.route('/fileUpload', methods=['GET', 'POST'])
@@ -121,4 +212,28 @@ def upload_file():
             if url_validation(url) == 0:
                 total_result.append(start_crawl(url))
         return render_template('result.html', result=total_result)
-    return error_message
+    return url_error_message
+
+
+@app.route('/get_similarity', methods=['GET', 'POST'])
+def sim_cal():
+    if request.method == 'POST':
+        target = request.form['url_i']
+        sim_list = []
+        sim = similarity_url(target)
+        for key in sim:
+            sim_list.append(total_result[key][0])
+        return render_template('result.html', sim_result=sim_list)
+    return "Similarity Error"
+
+
+@app.route('/get_tfidf', methods=['GET', 'POST'])
+def tfidf_cal():
+    if request.method == 'POST':
+        target = request.form['url_i']
+        tfidf_list = []
+        word = cal_tf_idf(word_list[target])
+        for key in word:
+            tfidf_list.append(key)
+        return render_template('result.html', tfidf_result=tfidf_list)
+    return "tfidf Error"
